2f707bfa2a31bcbfc1d8143b5a843eae9bbda925
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-07-27 09:59:46 -0500

    working on chrome storage

---
+++ popup.js
@@ -0,0 +1,40 @@
+const btnSave = document
+  .querySelector("#saveBtn")
+  .addEventListener("click", () => {
+    //Execution in the PopUp
+    const msg = document.getElementById("msg");
+    msg.innerText = "Mark saved";
+    let note = document.getElementById("note");
+    console.log(note.value);
+
+    //Execution in the DOM
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      console.log(tabs);
+
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: (note) => {
+          const hasVideo = document.getElementsByTagName("iframe");
+
+          if (hasVideo) {
+            console.log("The page has a video");
+            const progressBar = document.querySelector(".ytp-progress-bar");
+            let timeStamp = progressBar.getAttribute("aria-valuetext");
+
+            //Save timeStamp in chrome local storage
+            chrome.storage.local.get(["timeList"], (result) => {
+              if (result.timeList) {
+                console.log(result.timeList);
+              } else {
+                chrome.storage.local.set({ timeList: timeStamp }, () =>
+                  console.log(`${timeStamp}: Created in local storage`)
+                );
+              }
+            });
+          }
+        },
+      });
+    });
+
+    note.value = "";
+  });
