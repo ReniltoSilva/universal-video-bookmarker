725838fc646b2b67986e465ba178c1d2ebe49a79
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-07-29 12:28:38 +0700

    Trying to fix issue with localStorage

--- popup.js
+++ popup.js
@@ -4,26 +4,32 @@ const btnSave = document
     //Execution in the PopUp
     const msg = document.getElementById("msg");
     msg.innerText = "Mark saved";
-    let note = document.getElementById("note");
-
+    let note = document.getElementById("note").value;
+    
     //Execution in the DOM
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       console.log(tabs);
+      // console.log(note)
 
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
-        args: [note.value],
+        args: [note],
         func: (args) => {
           const hasVideo = document.getElementsByTagName("iframe");
-          console.log(args);
+          // console.log(args);
+          
+          
           if (hasVideo) {
             console.log("The page has a video");
+            console.log(args)
             const progressBar = document.querySelector(".ytp-progress-bar");
             let timeStamp = progressBar.getAttribute("aria-valuetext");
 
             //Save timeStamp in chrome local storage
             chrome.storage.local.get(["timeList"], (result) => {
               if (result.timeList) {
+                console.log(timeStamp)
+                console.log(result)
                 console.log(result.timeList);
               } else {
                 chrome.storage.local.set({ timeList: timeStamp }, () =>
@@ -31,7 +37,7 @@ const btnSave = document
                 );
               }
             });
-          }
+          }console.log('No video faound in the page')
         },
       });
     });
